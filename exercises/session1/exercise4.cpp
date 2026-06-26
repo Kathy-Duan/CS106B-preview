206ab9a9e1045af6246c516e1b87df685e9f8941
Kathy Duan <duank@stanford.edu>
2026-06-25 18:37:08 -0700

    feat: exercises 4 and 5 update

--- exercises/session1/exercise4.cpp
+++ exercises/session1/exercise4.cpp
@@ -5,6 +5,11 @@ using namespace std;
 int main() {
     int sum = 0;
     int n;
+    cout << "Number of times: ";
     cin >> n;
-    for (int i = 1; i)
+    for (int i = 1; i <= 2*n - 1; i += 2) {
+        sum += i;
+    }
+    cout << sum;
+    return 0;
 }
--- exercises/session1/exercise5.cpp
+++ exercises/session1/exercise5.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <limits.h>
 
 using namespace std;
 
 int main() {
     int number;
-    int max = 0;
+    int max = INT_MIN;  // -2147483648
     cin >> number;
     while (number != 0) {
+        cin >> number;
         if (number > max) {
             max = number;
         }
     } 
+    cout << max;
+    return 0;
 }
